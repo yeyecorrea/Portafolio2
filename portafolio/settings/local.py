7d3669e6082624e3448163939ccd01277e1f1e16
yeyecorrea <yeyecorrea838@gmail.com>
2023-07-16 16:36:23 -0500

    añadimos un nuevo provedor de base de datos con sus credenciales

--- portafolio/settings/local.py
+++ portafolio/settings/local.py
@@ -8,10 +8,20 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'C22Zv7sszGBeWZd0mNww',
+        'HOST': 'containers-us-west-174.railway.app',
+        'PORT': '7131'
     }
 }
 
