4327492f7804fd5ca7c1d9d358953d3c5a11167c
yeyecorrea <yeyecorrea838@gmail.com>
2023-07-16 13:55:09 -0500

    se agregan las migraciones y se actualiza el archivo de requerimientos

--- portafolio/settings/local.py
+++ portafolio/settings/local.py
@@ -10,12 +10,8 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'YBdN88e8ycRKkvMYQO0m',
-        'HOST': 'containers-us-west-162.railway.app',
-        'PORT': '6567'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
