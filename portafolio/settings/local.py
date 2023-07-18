b41360a443134b695234450ab7fae5540cdf8f3c
yeyecorrea <yeyecorrea838@gmail.com>
2023-07-17 20:57:52 -0500

    se cambian las configuracion de los archivos static

--- portafolio/settings/local.py
+++ portafolio/settings/local.py
@@ -29,6 +29,9 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
